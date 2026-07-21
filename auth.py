4ef093a2c513265c2aed7fd4b375d5c01294c041
Mohan Raj R <mohanraj210607@gmail.com>
2026-07-21 15:15:12 +0530

    auth.py

--- auth.py
+++ auth.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
-# Default Admin Credentials
 USERNAME = "admin"
-PASSWORD = "admin123"
+PASSWORD = "admin123@"
 
 
 def login():
@@ -14,20 +13,17 @@ def login():
     if st.button("Login"):
         if username == USERNAME and password == PASSWORD:
             st.session_state["logged_in"] = True
-            st.success("✅ Login Successful")
+            st.success("Login Successful")
             st.rerun()
         else:
-            st.error("❌ Invalid Username or Password")
+            st.error("Invalid Username or Password")
+
+
+def check_login():
+    return st.session_state.get("logged_in", False)
 
 
 def logout():
     if st.sidebar.button("🚪 Logout"):
         st.session_state["logged_in"] = False
         st.rerun()
-
-
-def check_login():
-    if "logged_in" not in st.session_state:
-        st.session_state["logged_in"] = False
-
-    return st.session_state["logged_in"]
